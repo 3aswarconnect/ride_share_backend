866a5ed5917119af6d7c6ce2d53e3bed2891f89a
Eshwar <142719089+3aswarconnect@users.noreply.github.com>
2024-07-22 21:08:34 +0530

    Update Ride.js

--- models/Ride.js
+++ models/Ride.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 const rideSchema = new mongoose.Schema({
   bike: String,
-  time: String,
+  time: Date,
   destination: String,
   fare: String,
   contactNumber: String
-});
+}, { timestamps: true }); // Adds createdAt and updatedAt fields
 
-module.exports = mongoose.model('Ride', rideSchema);
+const Ride = mongoose.model('Ride', rideSchema);
+
+module.exports = Ride;
